5151e347e6a1ad600450a5ff6a2e40120869526b
Jeff Golden <gjgolden08@gmail.com>
2020-11-19 18:30:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,11 @@ end
 
 def monopoly_with_second_tier
   monopoly = {:railroads => {}}
-  
+  monopoly[:railroads] = {
+    {:pieces => "4"}
+    {:rent_in_dollars => {}}
+    {:names => {}}
+  }
   monopoly 
   
      epic_tragedy[:additional_characters] = [
